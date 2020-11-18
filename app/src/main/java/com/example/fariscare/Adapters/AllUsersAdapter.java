7f09e2e89f215c5d811f775b1ca5ff473be4b554
Ming Wei Kwong <s10195172@connect.np.edu.sg>
2020-11-18 12:35:11 +0800

    View holders

---
+++ app/src/main/java/com/example/fariscare/Adapters/AllUsersAdapter.java
@@ -0,0 +1,60 @@
+package com.example.fariscare.Adapters;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.fariscare.Member;
+import com.example.fariscare.R;
+
+import java.util.ArrayList;
+
+public class AllUsersAdapter extends RecyclerView.Adapter<AllUsersAdapter.AllUsersViewHolder> {
+
+    Activity context;
+    ArrayList<Member> memberArrayList;
+
+    public AllUsersAdapter(Activity context, ArrayList<Member> memberArrayList){
+        this.context=context;
+        this.memberArrayList=memberArrayList;
+    }
+
+    @NonNull
+    @Override
+    public AllUsersViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.all_users,parent,false);
+        AllUsersViewHolder allUsersAdapter=new AllUsersViewHolder(view);
+        return allUsersAdapter;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AllUsersViewHolder holder, int position) {
+        Member member=memberArrayList.get(position);
+        holder.textViewName.setText(member.getName());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return memberArrayList.size();
+    }
+
+    public class AllUsersViewHolder extends RecyclerView.ViewHolder{
+
+        TextView textViewName;
+        Button button;
+        public AllUsersViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textViewName=(TextView)itemView.findViewById(R.id.itemName);
+            button=itemView.findViewById(R.id.callButton);
+
+        }
+    }
+
+}
--- app/src/main/java/com/example/fariscare/OCall.java
+++ app/src/main/java/com/example/fariscare/OCall.java
@@ -1,19 +1,40 @@
 package com.example.fariscare;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.sinch.android.rtc.PushPair;
+import com.sinch.android.rtc.Sinch;
+import com.sinch.android.rtc.SinchClient;
+import com.sinch.android.rtc.calling.Call;
+import com.sinch.android.rtc.calling.CallClient;
+import com.sinch.android.rtc.calling.CallClientListener;
+import com.sinch.android.rtc.calling.CallListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class OCall extends AppCompatActivity {
 
     RecyclerView contactsRecycler;
     FirebaseAuth auth;
     FirebaseUser firebaseUser;
+    SinchClient sinchClient;
+    Call call;
+    ArrayList<Member> memberArrayList;
+    DatabaseReference reference;
 
 
     @Override
@@ -24,7 +45,81 @@ public class OCall extends AppCompatActivity {
         contactsRecycler.setHasFixedSize(true);
         contactsRecycler.setLayoutManager(new LinearLayoutManager(this));
 
+        memberArrayList=new ArrayList<>();
+        reference= FirebaseDatabase.getInstance().getReference().child("Member");
         auth=FirebaseAuth.getInstance();
-        firebaseUser
+        firebaseUser =auth.getCurrentUser();
+
+
+        sinchClient= Sinch.getSinchClientBuilder().context(this)
+                .userId(firebaseUser.getUid())
+                .applicationKey("")
+                .applicationSecret("").environmentHost("").build();
+
+        sinchClient.setSupportCalling(true);
+        sinchClient.startListeningOnActiveConnection();
+
+
+        sinchClient.getCallClient().addCallClientListener(new SinchCallClientListener(){
+
+        });
+        sinchClient.start();
+
+        fetchAllUsers();
+    }
+
+    private void fetchAllUsers(){
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                memberArrayList.clear();
+                for (DataSnapshot des:dataSnapshot.getChildren()){
+                    Member member=des.getValue(Member.class);
+
+                    memberArrayList.add(member);
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+
+    private class SinchCallListener implements CallListener{
+
+        @Override
+        public void onCallProgressing(Call call) {
+            Toast.makeText(getApplicationContext(),"Ring ring",Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        public void onCallEstablished(Call call) {
+            Toast.makeText(getApplicationContext(),"Call connected",Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onCallEnded(Call endedcall) {
+            Toast.makeText(getApplicationContext(),"Call ended",Toast.LENGTH_SHORT).show();
+            call=null;
+            endedcall.hangup();
+        }
+
+        @Override
+        public void onShouldSendPushNotification(Call call, List<PushPair> list) {
+
+        }
+    }
+
+
+    private class SinchCallClientListener implements CallClientListener{
+
+        @Override
+        public void onIncomingCall(CallClient callClient, Call call) {
+
+        }
     }
 }
