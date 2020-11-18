4e80f80c2834d68884375c18d49ce2845a96f1ce
Ming Wei Kwong <s10195172@connect.np.edu.sg>
2020-11-18 09:39:44 +0800

    oCall Activity

---
+++ app/src/main/java/com/example/fariscare/oCall.java
@@ -0,0 +1,14 @@
+package com.example.fariscare;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class oCall extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_o_call);
+    }
+}
